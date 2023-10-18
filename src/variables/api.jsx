569b896102285251acb575335d8d91fe83141aad
margretheby <margretheby@users.noreply.github.com>
2023-10-18 09:58:29 +0200

    creating API hook
    Displaying venues on home page

---
+++ src/variables/api.jsx
@@ -0,0 +1,7 @@
+// Base url for API
+const BASE_URL = 'https://api.noroff.dev/api/v1/holidaze/'
+
+// Endpoints for venues, bookings and profiles
+export const venueUrl = `${BASE_URL + 'venues'}`
+export const bookingsUrl = `${BASE_URL + 'bookings'}`
+export const profilesUrl = `${BASE_URL + 'profiles'}`
