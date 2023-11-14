7bafdbb7375d9bd448ca2a9b70253bd227e0b922
margretheby <margretheby@users.noreply.github.com>
2023-11-14 09:40:21 +0100

    User feedback on update venue + avatar

--- src/components/UpdateProfileData/UpdateProfileData.jsx
+++ src/components/UpdateProfileData/UpdateProfileData.jsx
@@ -1,5 +1,7 @@
 import { accessToken } from '../../variables/localStorage.jsx'
 import { profileMediaUpdateUrl } from '../../variables/api.jsx'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 async function UpdateProfileMedia(avatar) {
     try {
@@ -13,6 +15,18 @@ async function UpdateProfileMedia(avatar) {
         });
 
         const result = await response.json();
+
+        if(response.ok) {
+            toast.success('Success! Your avatar is updated.', {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 3000,
+            });          
+        } else {
+            toast.error('Ooobs! Something went wrong, please try again.', {
+                position: toast.POSITION.TOP_CENTER,
+                autoClose: 3000,
+            });
+        }
 
         return result;
     } catch(error) {
