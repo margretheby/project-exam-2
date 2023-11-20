8a58c3bdb533e28e4b122ef2a8626473a54b8f1d
margretheby <margretheby@users.noreply.github.com>
2023-11-20 11:25:22 +0100

    Import font family

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
         <div className="flex justify-between items-top mx-4">
             <Nav />
             <div>
-                <p className="text-3xl mt-2">Holidaze</p>
+                <NavLink to='/' className="text-3xl mt-2 text-[#EFEFEF] brand">Holidaze</NavLink>
             </div>
             <NavLink to='/profile' >
                 <img src='/icons/user.png' alt='User page' className="h-8 mt-2"/>
