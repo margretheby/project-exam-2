e31c70a6a6e8217d8170b4b59268d24a1e650821
margretheby <margretheby@users.noreply.github.com>
2023-11-21 11:55:07 +0100

    New venue styles

--- src/pages/NewVenuePage.jsx
+++ src/pages/NewVenuePage.jsx
@@ -3,7 +3,7 @@ import CreateVenueForm from "../components/CreateVenueForm/CreateVenueForm";
 function NewVenue() {
     return (
       <div className="py-10 px-4 bg-gradient-to-t from-[#222222] to-[#222222aa] flex flex-col items-center">
-        <h1>Add venue</h1> 
+        <h1 className="text-2xl text-[#FFEC58]">Add new venue</h1> 
         <CreateVenueForm />
       </div>
     );
